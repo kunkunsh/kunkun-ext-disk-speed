f900f944d852d900fd355d000952a8e111e73c72
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-23 08:08:49 -0400

    Update package version to 0.0.8, add warning about read speed inaccuracies in README and App.svelte, and change default target directory to undefined in store.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - Store: https://kunkun.sh/store/disk-speed
 
 ![](https://i.imgur.com/8ISVrRe.png)
+
+This extension's read speed is not accurate. Your OS may cache the test data and result in a much higher speed.
+Will be fixed in the future.
--- src/App.svelte
+++ src/App.svelte
@@ -126,6 +126,10 @@
     <Button disabled={!$targetDir || running} on:click={startSpeedTest}>
       Start Speed Test
     </Button>
+    <small class="text-gray-500">
+      This extension's read speed may be inaccurate. Your OS may cache the test
+      data and result in a much higher speed. Will be fixed in the future.
+    </small>
     <div class="grid h-96 w-full grid-cols-2">
       <SpeedGauge
         speedInMBps={writeSpeedMBps}
