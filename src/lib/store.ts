969ba81090c7ca1097b168c65e6fd33e086ec7b2
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-23 08:27:04 -0400

    Update package version to 0.0.10, disable Deno support in VSCode settings, and modify target directory in store based on development environment.

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,7 @@
 import { writable } from "svelte/store";
+import { dev } from "$app/environment";
 
 export const stress = writable(1);
-export const targetDir = writable<string | undefined>("/Volumes/Portable2TB");
+export const targetDir = writable<string | undefined>(
+  dev ? "/Volumes/Portable2TB" : undefined
+);
