179d6095224b624c626c2cf28256fc8e8528c6e8
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-23 08:28:18 -0400

    Refactor target directory assignment in store to use import.meta.env for development environment detection.

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,6 @@
 import { writable } from "svelte/store";
-import { dev } from "$app/environment";
 
 export const stress = writable(1);
 export const targetDir = writable<string | undefined>(
-  dev ? "/Volumes/Portable2TB" : undefined
+  import.meta.env.DEV ? "/Volumes/Portable2TB" : undefined
 );
